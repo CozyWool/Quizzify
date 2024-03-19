4874897c8af76e5ba31e4db45aa30d05509f9f4e
thisfozz <kfcfozz@vk.com>
2024-03-19 12:11:52 +0500

    Был изменен формат скрипта с .txt на .sql Проведена работа над наименованием строк. Было убрано каскадное удаление вопроса при удалении User. Теперь при удалении User таблица SecretQuestions останется без изменений и наоборот при удалении значени из SecretQuestions - поле selected_secret_question_id у User станет NULL.

--- Quizzify/Quizzify/Scripts/Quizzify.sql
+++ Quizzify/Quizzify/Scripts/Quizzify.sql
@@ -1,4 +1,4 @@
-CREATE TABLE SecretQuestions 
+﻿CREATE TABLE SecretQuestions 
 (
     secret_q_id SERIAL PRIMARY KEY,
     question_text TEXT NOT NULL
@@ -6,21 +6,21 @@ CREATE TABLE SecretQuestions
 
 CREATE TABLE Users
 (
-    user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     login VARCHAR(20) NOT NULL UNIQUE,
     password_hash VARCHAR(256) NOT NULL,
     email VARCHAR(50) NOT NULL UNIQUE,
-    selected_secret_question_id INTEGER NOT NULL,
+    selected_secret_question_id INTEGER,
     secret_answer_hash VARCHAR(256) NOT NULL, 
     TwoFA_auth_method TEXT,
-	Google_Authorization BYTEA,
-    CONSTRAINT secret_question_fk FOREIGN KEY(selected_secret_question_id) REFERENCES SecretQuestions(secret_q_id) ON DELETE CASCADE
+    Google_Authorization BYTEA,
+    CONSTRAINT secret_question_fk FOREIGN KEY(selected_secret_question_id) REFERENCES SecretQuestions(secret_q_id) ON DELETE SET NULL
 );
 
 CREATE TABLE Players
 (
-    player_id_pk SERIAL PRIMARY KEY,
-    user_id uuid UNIQUE NOT NULL,
+    player_id SERIAL PRIMARY KEY,
+    user_id UUID UNIQUE NOT NULL,
     nickname VARCHAR(20) NOT NULL,
     user_profile_picture BYTEA, 
     about TEXT,
@@ -29,7 +29,7 @@ CREATE TABLE Players
 
 CREATE TABLE Packages
 (
-    package_id_pk SERIAL PRIMARY KEY,
+    package_id SERIAL PRIMARY KEY,
     package_name VARCHAR(50) NOT NULL,
     created_at DATE DEFAULT CURRENT_DATE,
     difficulty INTEGER NOT NULL CHECK (difficulty >= 1 AND difficulty <= 10)
@@ -37,16 +37,16 @@ CREATE TABLE Packages
 
 CREATE TABLE Rounds
 (
-    round_id_pk SERIAL PRIMARY KEY,
+    round_id SERIAL PRIMARY KEY,
     package_id INTEGER NOT NULL,
     name VARCHAR(255) NOT NULL,
-    type_rounds VARCHAR(7) NOT NULL CHECK(type_rounds ILIKE 'Обычный' or type_rounds ILIKE 'Финал'),
-    CONSTRAINT rounds_package_id_fk FOREIGN KEY (package_id) REFERENCES Packages(package_id_pk) ON DELETE CASCADE
+    round_type VARCHAR(7) NOT NULL CHECK(round_type ILIKE 'Обычный' or round_type ILIKE 'Финал'),
+    CONSTRAINT rounds_package_id_fk FOREIGN KEY (package_id) REFERENCES Packages(package_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Questions
 (
-    question_id_pk SERIAL PRIMARY KEY,
+    question_id SERIAL PRIMARY KEY,
     round_id INTEGER NOT NULL,
     question_text TEXT,
     question_theme VARCHAR(15),
@@ -55,7 +55,7 @@ CREATE TABLE Questions
     question_comment TEXT,
     answer_text TEXT,
     answer_image_url BYTEA,
-    CONSTRAINT questions_round_id_fk FOREIGN KEY (round_id) REFERENCES Rounds(round_id_pk) ON DELETE CASCADE,
+    CONSTRAINT questions_round_id_fk FOREIGN KEY (round_id) REFERENCES Rounds(round_id) ON DELETE CASCADE,
     CONSTRAINT at_least_one_question CHECK(question_text IS NOT NULL OR question_image_url IS NOT NULL),
     CONSTRAINT at_least_one_answer CHECK(answer_text IS NOT NULL OR answer_image_url IS NOT NULL)
 );

1e8c2ffda2b616d45bb265319479488975fd6748
Mirovinkin <137717625+Mirovinkin@users.noreply.github.com>
2024-03-25 23:11:11 +0500

    Feature-17
    Добавил команды

---
+++ Commands/Commands.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Command_
+{
+    public abstract class Command : ICommand
+    {
+        bool ICommand.CanExecute(object parameter) => CanExecute(parameter);
+
+        void ICommand.Execute(object parameter) => Execute(parameter);
+
+        protected virtual bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        protected abstract void Execute(object? parameter = null);
+
+        protected virtual void OnCanExecuteChanged(EventArgs e)
+        {
+            CanExecuteChanged?.Invoke(this, e);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            OnCanExecuteChanged(EventArgs.Empty);
+        }
+
+        public event EventHandler? CanExecuteChanged;
+    }
+}
---
+++ Commands/DelegateCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Command_
+{
+    public class DelegateCommand : Command
+    {
+        private readonly Func<object, bool> _canExecuteMethod;
+        private readonly Action<object> _executeMethod;
+
+        public DelegateCommand(Action<object> executeMethod, Func<object, bool> canExecuteMethod)
+        {
+            _canExecuteMethod = canExecuteMethod;
+            _executeMethod = executeMethod;
+        }
+
+        protected override bool CanExecute(object parameter)
+        {
+            return _canExecuteMethod(parameter);
+        }
+
+        protected override void Execute(object parameter = null)
+        {
+            _executeMethod(parameter);
+        }
+    }
+}

22ca6068d7e3736a606a1bb9b1c6972516808af1
thisfozz <kfcfozz@vk.com>
2024-03-27 16:35:26 +0500

    Написал модельки для "Пакет вопросов" и "Регистрация/авторизация"

---
+++ Quizzify/Quizzify/Model/Users/RegistrationModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Quizzify.Model.Users;
+public class RegistrationModel
+{
+    [Required(ErrorMessage = "Логин обязателен")]
+    public string Login { get; set; }
+
+    [Required(ErrorMessage = "Пароль обязателен")]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Электронная почта обязательна")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Выбор секретного вопроса обязателен")]
+    public int SelectedSecretQuestionId { get; set; }
+
+    [Required(ErrorMessage = "Ответ на секретный вопрос обязателен")]
+    public string SecretAnswer { get; set; }
+}

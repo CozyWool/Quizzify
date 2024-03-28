c3f97adf84bbee0b6f74e3beb83ad7ebf668a572
CozyWool <vsergeev201530@gmail.com>
2024-03-28 23:22:19 +0500

    Начал писать логику сервера

--- Quizzify/Quizzify.Client/View/MainView.xaml.cs
+++ Quizzify/Quizzify.Client/View/MainView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Windows;
+using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Configuration;
 using Quizzify.Client.ViewModel;
 using Quizzify.DataAccess.Contexts;
@@ -8,12 +9,14 @@ namespace Quizzify.Client.View;
 
 public partial class MainView : Window
 {
+    private HubConnection _connection;
+
     public MainView(MainViewModel viewModel)
     {
         InitializeComponent();
         DataContext = viewModel;
     }
-    
+
     // TODO: Выпилить в класс модели
     // private static IConfiguration GetConnectionString()
     // {
@@ -22,4 +25,11 @@ public partial class MainView : Window
     //     builder.AddJsonFile("appsettings.json");
     //     return builder.Build();
     // }
+    private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        _connection = new HubConnectionBuilder()
+            .WithUrl($"https://localhost:{PortTextBox.Text}/host")
+            .Build();
+        await _connection.StartAsync();
+    }
 }

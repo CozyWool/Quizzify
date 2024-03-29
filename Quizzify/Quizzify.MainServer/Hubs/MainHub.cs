c7e523c3612fc78d920badc02730bde353893d08
DmitriyYakubovskiy <93999486+DmitriyYakubovskiy@users.noreply.github.com>
2024-03-29 14:02:48 +0500

    Начало реализации логики main серверы

---
+++ Quizzify/Quizzify.MainServer/Hubs/MainHub.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Quizzify.DataAccess.Contexts;
+
+namespace Quizzify.MainServer.Hubs
+{
+    public class MainHub:Hub
+    {
+        //private DatabaseManager databaseManager;
+
+        //public MainHub()
+        //{
+        //    //databaseManager = new DatabaseManager();
+        //}
+
+        public async Task AutorizeSend(string name, string password)
+        {
+            await Clients.Caller.SendAsync("ReceiveAutorize", LoginVerification(name, password));
+        }
+
+        public async Task RegistrationSend(string name, string password)
+        {
+            await Clients.Caller.SendAsync("ReceiveRegistration", RegistrationVerification(name, password));
+        }
+
+        private bool LoginVerification(string name, string password)
+        {
+            //взаимодействие с бд
+            return true;
+        }
+
+        private bool RegistrationVerification(string name, string password)
+        {
+            var userEntity=ConvertTo
+            using var context = new DbQuizzifyContext(configuration);
+            context.AddUser(userEntity);
+        }
+    }
+}

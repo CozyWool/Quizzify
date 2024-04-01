9b940c03b8891a016ea72e51d258c7d4f6ae4981
DmitriyYakubovskiy <93999486+DmitriyYakubovskiy@users.noreply.github.com>
2024-04-01 19:17:28 +0500

    Добавил подключение к игре
    добавил эвенты к подключениям к серверам
    сделал пару методов для ClientHost
    сделал подключение к игре
    надо посмотреть переход из авторизейшен модел в плайер модел

---
+++ Quizzify/Quizzify.Client/Services/ClientHostHubService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Quizzify.Client.Model.Users;
+
+namespace Quizzify.Client.Services;
+
+public class ClientHostHubService
+{
+    public event Action<PlayerModel> InfoNewPlayerArrived;
+    private readonly HubConnection connection;
+
+    public ClientHostHubService(HubConnection connection)
+    {
+        this.connection = connection;
+    }
+
+    public async Task Connect()
+    {
+        await connection.StartAsync();
+    }
+
+    public void ReceivePlayerInfo()
+    {
+        connection.On<PlayerModel>("SendPlayersInfo", (player) =>
+        {
+            InfoNewPlayerArrived.Invoke(player);
+        });
+    }
+
+    public async Task SendPlayerInfo(PlayerModel player)
+    {
+        await connection.SendAsync("RecievePlayer", player); //TODO пока отправляю модель, сервер прнимает ентити, думаю там нужно сделать прием модели
+    }
+}
--- Quizzify/Quizzify.Client/ViewModel/MainViewModel.cs
+++ Quizzify/Quizzify.Client/ViewModel/MainViewModel.cs
@@ -1,6 +1,75 @@
-﻿namespace Quizzify.Client.ViewModel;
+﻿using Quizzify.Client.Command;
+using Quizzify.Client.Model.Users;
+using Quizzify.Client.Services;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
 
-public class MainViewModel
+namespace Quizzify.Client.ViewModel;
+
+public class MainViewModel : INotifyPropertyChanged
 {
-    
+    public ICommand ConnectToGameCommand { get; }
+    private List<PlayerModel> _players;
+    private PlayerModel _player;
+
+    private string _ip;
+    public string IP
+    {
+        get => _ip;
+        set
+        {
+            _ip = value;
+            OnPropertyChanged(nameof(IP));
+        }
+    }
+
+    private string _port;
+    public string Port
+    {
+        get => _port;
+        set
+        {
+            _port = value;
+            OnPropertyChanged(nameof(Port));
+        }
+    }
+
+    public MainViewModel(PlayerModel player)
+    {
+        this._player= player;
+        ConnectToGameCommand = new GenericCommand<object>(ConnectToGame);
+    }
+
+    public async void ConnectToGame(object obj)
+    {
+        try
+        {
+            var connection = App.HubConnectionConfiguration("localhost", 5098, "host");
+            var signal = new ClientHostHubService(connection);
+            await signal.Connect();
+            await signal.SendPlayerInfo(_player);
+            signal.ReceivePlayerInfo();
+            signal.InfoNewPlayerArrived += AddPlayer;
+            //TODO --- обновление view
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    private void AddPlayer(PlayerModel playerModel)
+    {
+        if (playerModel == null) return;
+        if (_players.Contains(playerModel)) return;
+        _players.Add(playerModel);
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }

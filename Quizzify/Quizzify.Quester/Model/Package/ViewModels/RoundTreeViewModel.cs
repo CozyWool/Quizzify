08a95b34968cbeffc9e4834dd2c09ee6684f1dbf
thisfozz <kfcfozz@vk.com>
2024-04-02 15:40:36 +0500

    Добавил мапер. PackageModel -> PackageTreeViewModel

---
+++ Quizzify/Quizzify.Quester/Model/Package/ViewModels/RoundTreeViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Quizzify.Quester.Model.Package.ViewModels;
+public class RoundTreeViewModel
+{
+    public string Name { get; set; }
+    public List<QuestionTreeViewModel> Questions { get; set; }
+}
--- Quizzify/Quizzify.Quester/ViewModel/QuesterViewModel.cs
+++ Quizzify/Quizzify.Quester/ViewModel/QuesterViewModel.cs
@@ -1,21 +1,30 @@
-using System.IO;
+﻿using System.IO;
 using Quizzify.Quester.Model.Package;
 using System.Text;
 using System.Windows.Input;
 using Microsoft.Win32;
 using Newtonsoft.Json;
+using System.Windows;
+using Quizzify.Quester.Model.Package.ViewModels;
+using AutoMapper;
+using Quizzify.Quester.Mappers;
 
 
 namespace Quizzify.Quester.ViewModel;
 
 public class QuesterViewModel
 {
+    private readonly IMapper _mapper;
+
     public ICommand SaveToFileSerializedCommand { get; }
-    public ICommand UploadTpFileSerializedCommand { get; }
+    public ICommand UploadFileDeserializeCommand { get; }
 
     public QuesterViewModel()
     {
         SaveToFileSerializedCommand = new GenericCommand<PackageModel>(SaveToFile);
+        UploadFileDeserializeCommand = new GenericCommand<PackageModel>(UploadFile);
+        var config = new MapperConfiguration(cfg => cfg.AddProfile<MappingPackage>());
+        _mapper = config.CreateMapper();
     }
 
     private async Task SaveToFile(PackageModel packageModel)
@@ -25,6 +34,38 @@ public class QuesterViewModel
         if (saveFile.ShowDialog() == true)
         {
             await File.WriteAllTextAsync(saveFile.FileName, jsonSerialized, Encoding.UTF8);
+        }
+    }
+
+    private async Task UploadFile(PackageModel packageModel)
+    {
+        var openFileDialog = new OpenFileDialog();
+        openFileDialog.Filter = "JSON files (*.json)|*.json";
+
+        if (openFileDialog.ShowDialog() == true)
+        {
+            string jsonData = await File.ReadAllTextAsync(openFileDialog.FileName);
+            try
+            {
+                var package = JsonConvert.DeserializeObject<PackageModel>(jsonData);
+
+                try
+                {
+                    var packageTreeView = _mapper.Map<PackageTreeViewModel>(package);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка преобразования пакета: {ex.Message}");
+                }
+            } 
+            catch (JsonException ex)
+            {
+                MessageBox.Show($"Некорректный JSON: {ex.Message}") ;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке файла: {ex.Message}");
+            }
         }
     }
 }

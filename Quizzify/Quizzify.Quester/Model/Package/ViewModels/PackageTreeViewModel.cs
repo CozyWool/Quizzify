4ff17b7621aca8a509abeee1cbc646e0a791f18c
thisfozz <kfcfozz@vk.com>
2024-04-02 18:16:27 +0500

    Мелкий рефакторинг.

--- Quizzify/Quizzify.Quester/Model/Package/ViewModels/PackageTreeViewModel.cs
+++ Quizzify/Quizzify.Quester/Model/Package/ViewModels/PackageTreeViewModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace Quizzify.Quester.Model.Package.ViewModels;
 public class PackageTreeViewModel
 {
-    public string Name { get; set; }
+    public string PackageName { get; set; }
     public List<RoundTreeViewModel> Rounds { get; set; }
 }

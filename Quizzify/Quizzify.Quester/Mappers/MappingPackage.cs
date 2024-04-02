c34b17d6ced5ceeb60a3b2d868c67dd4065eaf43
thisfozz <kfcfozz@vk.com>
2024-04-02 15:40:36 +0500

    Добавил мапер. PackageModel -> PackageTreeViewModel

---
+++ Quizzify/Quizzify.Quester/Mappers/MappingPackage.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Quizzify.Quester.Model.Package;
+using Quizzify.Quester.Model.Package.ViewModels;
+
+namespace Quizzify.Quester.Mappers;
+
+public class MappingPackage : Profile
+{
+    public MappingPackage()
+    {
+        CreateMap<PackageTreeViewModel, PackageModel>()
+            .ForMember(dest => dest.PackageName, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Rounds, opt => opt.MapFrom(src => src.Rounds));
+
+        CreateMap<RoundTreeViewModel, RoundModel>();
+        CreateMap<QuestionTreeViewModel, QuestionModel>();
+    }
+}
---
+++ Quizzify/Quizzify.Quester/Model/Package/ViewModels/QuestionTreeViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Quizzify.Quester.Model.Package.ViewModels;
+public class QuestionTreeViewModel
+{
+    public string QuestionText { get; set; }
+    public int QuestionCost { get; set; }
+}

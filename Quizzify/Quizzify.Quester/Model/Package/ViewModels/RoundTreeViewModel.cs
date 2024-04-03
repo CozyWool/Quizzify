33b759bdc04f29464ed2bf6c3f0fb1f3b1a41f4d
thisfozz <kfcfozz@vk.com>
2024-04-03 23:12:50 +0500

    Привязка "Quizzify.Infrastructure.WPF" к "Quizzify.Quester", добавил Маппер для моделей и сами модели.

---
+++ Quizzify/Quizzify.Quester/Model/Package/ViewModels/RoundTreeViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Quizzify.Quester.Model.Package.ViewModels;
+public class RoundTreeViewModel
+{
+    public string RoundName { get; set; }
+    public List<ThemeTreeViewModel> Themes { get; set; }
+}

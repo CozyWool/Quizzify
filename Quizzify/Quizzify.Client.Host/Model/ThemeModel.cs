99152899ac3ec604afca71c0965584023408ea09
CozyWool <vsergeev201530@gmail.com>
2024-04-03 21:58:58 +0500

    Мелкие правки

--- Quizzify/Quizzify.Client.Host/Model/ThemeModel.cs
+++ Quizzify/Quizzify.Client.Host/Model/ThemeModel.cs
@@ -1,7 +1,11 @@
-﻿namespace Quizzify.Client.Host.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Quizzify.Client.Host.Model;
 public class ThemeModel
 {
     public int ThemeId { get; set; }
+    [Required(ErrorMessage = "Название темы обязателен")]
     public string ThemeName { get; set; }
+    [Required(ErrorMessage = "Cписок вопросов обязателен")]
     public List<QuestionModel> Questions { get; set; }
 }

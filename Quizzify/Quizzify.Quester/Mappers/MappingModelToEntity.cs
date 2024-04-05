5ab29d16639fbf13af0bc4940a342479000e6c05
Mirovinkin <137717625+Mirovinkin@users.noreply.github.com>
2024-04-05 18:16:04 +0500

    Добавил Мапер и публикацию в бд

---
+++ Quizzify/Quizzify.Quester/Mappers/MappingModelToEntity.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Quizzify.DataAccess.Entities;
+using Quizzify.Quester.Model.Package;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizzify.Quester.Mappers
+{
+    public class MappingModelToEntity : Profile
+    {
+
+        public MappingModelToEntity() 
+        {
+            CreateMap<PackageModel, PackageEntity>()
+                .ForMember(dest => dest.PackageId, opt => opt.MapFrom(src => src.PackageId))
+                .ForMember(dest => dest.PackageName, opt => opt.MapFrom(src => src.PackageName))
+                .ForMember(dest => dest.Difficulty, opt => opt.MapFrom(src => src.Difficulty))
+                .ForMember(dest => dest.Rounds, opt => opt.MapFrom(src => src.Rounds));
+
+            CreateMap<RoundModel, RoundEntity>()
+                .ForMember(dest => dest.RoundId, opt => opt.MapFrom(src => src.RoundId))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.RoundName))
+                .ForMember(dest => dest.RoundType, opt => opt.MapFrom(src => src.RoundType))
+                .ForMember(dest => dest.Themes, opt => opt.MapFrom(src => src.Themes));
+
+            CreateMap<ThemeModel, ThemeEntity>()
+                .ForMember(dest => dest.ThemeId, opt => opt.MapFrom(src => src.ThemeId))
+                .ForMember(dest => dest.ThemeName, opt => opt.MapFrom(src => src.ThemeName))
+                .ForMember(dest => dest.Questions, opt => opt.MapFrom(src => src.Questions));
+
+            CreateMap<QuestionModel, QuestionEntity>()
+                .ForMember(dest => dest.QuestionId, opt => opt.MapFrom(src => src.QuestionId))
+                .ForMember(dest => dest.QuestionCost, opt => opt.MapFrom(src => src.QuestionCost))
+                .ForMember(dest => dest.QuestionText, opt => opt.MapFrom(src => src.QuestionText))
+                .ForMember(dest => dest.QuestionImageUrl, opt => opt.MapFrom(src => src.QuestionImageUrl))
+                .ForMember(dest => dest.QuestionComment, opt => opt.MapFrom(src => src.QuestionComment))
+                .ForMember(dest => dest.AnswerText, opt => opt.MapFrom(src => src.AnswerText))
+                .ForMember(dest => dest.AnswerImageUrl, opt => opt.MapFrom(src => src.AnswerImageUrl));
+
+
+
+        }
+    }
+}
--- Quizzify/Quizzify.Quester/ViewModel/QuesterViewModel.cs
+++ Quizzify/Quizzify.Quester/ViewModel/QuesterViewModel.cs
@@ -16,6 +16,9 @@ using System.Windows.Documents;
 using System.Windows.Controls.Primitives;
 using Quizzify.Quester.View;
 using System.Collections.ObjectModel;
+using Quizzify.DataAccess.Entities;
+using Quizzify.DataAccess.Contexts;
+using Microsoft.Extensions.Configuration;
 
 namespace Quizzify.Quester.ViewModel;
 
@@ -25,7 +28,8 @@ public class QuesterViewModel : INotifyPropertyChanged
     private readonly IMapper _mapper;
 
     private PackageModel package;
-    private int count = 0;
+
+    private readonly IConfiguration _configuration;
 
     public ICommand SaveToFileSerializedCommand { get; set; }
     public ICommand UploadFileDeserializeCommand { get; }
@@ -35,7 +39,8 @@ public class QuesterViewModel : INotifyPropertyChanged
     public ICommand AddThemeCommand { get; set; }
     public ICommand AddQuestionCommand { get; set; }
 
-    public ICommand RoundSelectionChangedCommand { get; set; }
+
+    public ICommand PublicationToDBCommand { get; set; }
 
     private string _packageNameTextBox;
     public string PackageNameTextBox
@@ -172,7 +177,7 @@ public class QuesterViewModel : INotifyPropertyChanged
         }
     }
 
-    public QuesterViewModel()
+    public QuesterViewModel( IConfiguration configuration)
     {
         SaveToFileSerializedCommand = new GenericCommand<PackageModel>(async (model) => await SaveToFile(model));
         UploadFileDeserializeCommand = new GenericCommand<PackageModel>(async (model) => await UploadFile(model));
@@ -181,15 +186,17 @@ public class QuesterViewModel : INotifyPropertyChanged
         AddRoundCommand = new GenericCommand<PackageModel>(AddRound);
         AddThemeCommand = new GenericCommand<PackageModel>(AddTheme);
         AddQuestionCommand = new GenericCommand<PackageModel>(AddQuestion);
-        RoundSelectionChangedCommand = new GenericCommand<object>(roundComboBox_SelectionChanged);
-
+        PublicationToDBCommand = new GenericCommand<PackageModel>(PublishToDB);
 
         var config = new MapperConfiguration(cfg => cfg.AddProfile<MappingPackage>());
         _mapper = config.CreateMapper();
 
         package = new PackageModel();
         package.Rounds = new List<RoundModel>();
+        _configuration=configuration;
     }
+
+
 
     private async Task SaveToFile(PackageModel packageModel)
     {
@@ -199,6 +206,13 @@ public class QuesterViewModel : INotifyPropertyChanged
         {
             await File.WriteAllTextAsync(saveFile.FileName, jsonSerialized, Encoding.UTF8);
         }
+    }
+
+    private void PublishToDB(PackageModel packageModel)
+    {
+        var packageEntity= _mapper.Map<PackageEntity>(packageModel);
+        using var context = new DbQuizzifyContext(_configuration);
+        context.AddPackage(packageEntity);
     }
 
     private async Task UploadFile(PackageModel packageModel)
